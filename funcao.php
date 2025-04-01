216f05ed3ca3b4d64e7b86a4ed2a24113cf74617
Diego Vitorino <gabrielvitorino551@gmail.com>
2025-04-01 15:19:31 -0300

    Erro nos calculos de soma
    Alteração para deixar com as 4 op

--- funcao.php
+++ funcao.php
@@ -7,20 +7,20 @@ function soma($numero1, $numero2) {
 }
 function subtracao($numero1, $numero2) {
     
-    $total = $numero1+$numero2;
+    $total = $numero1-$numero2;
    
     return $total;
 }
    function divisao($numero1, $numero2) {
     
-    $total = $numero1+$numero2;
+    $total = $numero1/$numero2;
    
     return $total;
 }
 
    function multiplicacao($numero1, $numero2) {
     
-    $total = $numero1+$numero2;
+    $total = $numero1*$numero2;
    
     return $total;
 }
